843d7ff0bb55df511d5206433823b6fdd2531848
Leonardo Lage <88287253+Leonerdo15@users.noreply.github.com>
2023-05-18 10:11:58 +0100

    tirar ponto

--- models/pointModels.js
+++ models/pointModels.js
@@ -33,9 +33,9 @@ module.exports.getPointByRouteId = async function (id) {
     }
 }
 
-module.exports.createPoint = async function (po_location, po_altitude, po_velocity, po_TempOutside, po_TempInside, po_gx, po_gy, po_gz, po_ax, po_ay, po_az, po_DistUltraSound, po_ro_id) {
+module.exports.createPoint = async function (po_location, po_velocity, po_TempOutside, po_gx, po_gy, po_gz, po_ax, po_ay, po_az, po_DistUltraSound, po_ro_id) {
     try {
-        let sql = `insert into points (po_location, po_altitude, po_velocity, "po_TempOutside", "po_TempOutside", po_gx, po_gy, po_gz, po_ax, po_ay, po_az, "po_DistUltraSound", po_ro_id) values (POINT(ST_X('${po_location}'), ST_Y('${po_location}')), '${po_altitude}', '${po_velocity}', '${po_TempOutside}', '${po_TempInside}', '${po_gx}', '${po_gy}', '${po_gz}', '${po_ax}', '${po_ay}', '${po_az}', '${po_DistUltraSound}', '${po_ro_id}') returning *`
+        let sql = `insert into points (po_velocity, "po_TempInside", po_gx, po_gy, po_gz, po_ax, po_ay, po_az, "po_DistUltraSound", po_ro_id) values ( ${po_velocity}, ${po_TempOutside}, ${po_gx}, ${po_gy}, ${po_gz}, ${po_ax}, ${po_ay}, ${po_az}, ${po_DistUltraSound}, ${po_ro_id}) returning *`
         let result = await pool.query(sql)
         return {status: 200, data: result.rows[0]}
     } catch(e) {
--- routes/pointRouter.js
+++ routes/pointRouter.js
@@ -24,9 +24,7 @@ router.get('/ro_id/:id(\\d+)', async function (req, res, next) {
 router.post('/', async function (req, res, next) {
     let newPoint = req.body;
     let po_location = newPoint.po_location;
-    let po_altitude = newPoint.po_altitude;
     let po_velocity = newPoint.po_velocity;
-    let po_TempOutside = newPoint.po_TempOutside;
     let po_TempInside = newPoint.po_TempInside;
     let po_gx = newPoint.po_gx;
     let po_gy = newPoint.po_gy;
@@ -36,7 +34,7 @@ router.post('/', async function (req, res, next) {
     let po_az = newPoint.po_az;
     let po_DistUltraSound = newPoint.po_DistUltraSound;
     let po_ro_id = newPoint.po_ro_id;
-    let result = await pointModels.createPoint(po_location, po_altitude, po_velocity, po_TempOutside, po_TempInside, po_gx, po_gy, po_gz, po_ax, po_ay, po_az, po_DistUltraSound, po_ro_id);
+    let result = await pointModels.createPoint(po_location, po_velocity, po_TempInside, po_gx, po_gy, po_gz, po_ax, po_ay, po_az, po_DistUltraSound, po_ro_id);
     res.status(result.status).send(result.data);
 });
 
